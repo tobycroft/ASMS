e938b86c5113697a91f040b81ea3a8bbb68fee6c
Tuuz <silverhawk@foxmail.com>
2021-02-02 16:00:17 +0800

    TuuzPC

--- app/asms/model/ProjectModel/ProjectModel.go
+++ app/asms/model/ProjectModel/ProjectModel.go
@@ -22,3 +22,18 @@ func Api_find(token interface{}) gorose.Data {
 		return ret
 	}
 }
+
+func Api_dec_amount(pid interface{}) bool {
+	db := tuuz.Db().Table(table)
+	where := map[string]interface{}{
+		"pid": pid,
+	}
+	db.Where(where)
+	_, err := db.Increment("amount", 1)
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return false
+	} else {
+		return true
+	}
+}
