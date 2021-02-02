d4e8c258af908d1aef07176d1ec446b5f137a928
Tuuz <silverhawk@foxmail.com>
2021-02-02 15:58:44 +0800

    TuuzPC

---
+++ app/asms/action/Send.go
@@ -0,0 +1,37 @@
+package action
+
+import (
+	"fmt"
+	config2 "github.com/sunnyos/tencentSms/config"
+	"github.com/sunnyos/tencentSms/sms"
+	"main.go/app/asms/model/LogErrorModel"
+	"main.go/app/asms/model/TencentModel"
+	"main.go/tuuz"
+	"main.go/tuuz/Calc"
+	"main.go/tuuz/Log"
+)
+
+func App_tencent(id interface{}, phone, quhao, code string) {
+	tencent := TencentModel.Api_find(id)
+	if len(tencent) > 0 {
+		config := &config2.Config{
+			AppId:  Calc.Any2String(tencent["appid"]),
+			AppKey: Calc.Any2String(tencent["appkey"]),
+			Sign:   Calc.Any2String(tencent["sign"]),
+		}
+		s := sms.NewSms(config)
+		res, err := s.GetSmsSender().Fetchs(phone, quhao, []string{code}, tencent["861810"].(int64))
+		if err != nil {
+			Log.Crrs(err, tuuz.FUNCTION_ALL())
+		}
+		if res.Result == 0 {
+
+		} else {
+			LogErrorModel.Api_insert(tencent["pid"], res.Errmsg)
+		}
+		fmt.Println(res.Errmsg, res.Ext, res.Result, err)
+	} else {
+
+	}
+
+}
