53bfc9990d17060df73b4df07398d26b2f289ea1
Tuuz <miracle-plus@outlook.com>
2021-04-16 18:24:37 +0800

    TuuzMac

---
+++ app/asms/model/IhuyiModel/IhuyiModel.go
@@ -0,0 +1,24 @@
+package IhuyiModel
+
+import (
+	"github.com/gohouse/gorose/v2"
+	"main.go/tuuz"
+	"main.go/tuuz/Log"
+)
+
+const table = "ihuyi"
+
+func Api_find(id interface{}) gorose.Data {
+	db := tuuz.Db().Table(table)
+	where := map[string]interface{}{
+		"id": id,
+	}
+	db.Where(where)
+	ret, err := db.First()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return nil
+	} else {
+		return ret
+	}
+}
